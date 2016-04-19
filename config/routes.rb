8aa7819a7c5898d43bc11ab5d1b058643db64dbd
Nathan Goodman <Nate@Nathans-MacBook-Air.local>
2016-04-19 11:32:46 -0400

    add model tests and apps model, controller, fix hound

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
 
-    resources :procedures do
-      resources :levels, only: [:create, :delete]
-    end
-    resources :stages do
-      resources :materials, only: [:create, :delete]
-    end
-    resources :documents
-    resources :apps
+  resources :procedures do
+    resources :levels, only: [:create, :delete]
+  end
+  resources :stages do
+    resources :materials, only: [:create, :delete]
+  end
+  resources :documents
+  resources :apps
   # Serve websocket cable requests in-procedure
   # mount ActionCable.server => '/cable'
   root 'apps#index'
